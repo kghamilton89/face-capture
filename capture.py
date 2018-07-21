dab37517ce59f53050fd18bb490cad0d48eeb005
KG Hamilton <kenneth.hamilton@axibase.com>
2018-07-21 13:13:58 +0300

    Update capture.py

--- capture.py
+++ capture.py
@@ -8,27 +8,25 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
 # Create variables. `img` contains the image to analyze and `gray` converts the image to grayscale
-img = cv.imread('test.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+image = cv.imread('test.jpg')
+convert = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # OpenCV function for facial recognition.
-faces = face_cascade.detectMultiScale(gray, 1.9, 5)
+face = face_cascade.detectMultiScale(convert, 1.9, 5)
 
 # Label faces.
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    gray_main = gray[y:y+h, x:x+w]
-    color_main = img[y:y+h, x:x+w]
+for (x,y,w,h) in face:
+    cv.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    grayscale = convert[y:y+h, x:x+w]
+    original = image[y:y+h, x:x+w]
     
     # OpenCV function for eye recognition
-    eyes = eye_cascade.detectMultiScale(gray_main)
+    eyes = eye_cascade.detectMultiScale(grayscale)
     for (ex,ey,ew,eh) in eyes:
-        cv.rectangle(color_main,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        
-#cv.imshow('gray', gray)
+        cv.rectangle(original,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-# Print image with labeling.
-cv.imshow('img',img)	
-cv.imwrite('res.jpg',img)
+# Print and write image with labeling.
+cv.imshow('image',image)	
+cv.imwrite('res.jpg',image)
 cv.waitKey(0)
 cv.destroyAllWindows()
