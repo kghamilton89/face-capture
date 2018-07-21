12161913a2aae1daf924593fef0cf92a5a975d2d
KG Hamilton <kenneth.hamilton@axibase.com>
2018-07-21 11:20:52 +0300

    Update capture.py

--- capture.py
+++ capture.py
@@ -7,9 +7,9 @@ import cv2 as cv
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
-# Create variables. `image` contains the image to analyze and `grayscale` converts the image to grayscale
-image = cv.imread('test.jpg')
-grayscale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# Create variables. `img` contains the image to analyze and `gray` converts the image to grayscale
+img = cv.imread('test.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # OpenCV function for facial recognition.
 faces = face_cascade.detectMultiScale(gray, 1.9, 5)
@@ -28,6 +28,6 @@ for (x,y,w,h) in faces:
 #cv.imshow('gray', gray)
 
 # Print image with labeling.
-cv.imshow('img',image)	
+cv.imshow('img',img)	
 cv.waitKey(0)
 cv.destroyAllWindows()
